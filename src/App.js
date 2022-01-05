5f50d8a1a1db7c1c50708206bb74982e4a42467c
AndriusVegan <lifecoachandy7@gmail.com>
2022-01-05 17:22:36 +0200

    final part, punk selection functionality implemented

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Header from "./components/Header";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import PunkList from "./components/PunkList";
+import Main from "./components/Main";
 
 function App() {
   const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
 
   useEffect(() => {
     const getMyNfts = async () => {
@@ -22,13 +24,22 @@ function App() {
   return (
     <div className="app">
       <Header />
+      {punkListData.length > 0 && (
+        <>
+          <Main punkListData={punkListData} selectedPunk={selectedPunk} />
+          <PunkList
+            punkListData={punkListData}
+            setSelectedPunk={setSelectedPunk}
+          />
+        </>
+      )}
+
       {/* <CollectionCard
         id={0}
         name={"VeganPunk"}
         traits={[{ value: 7 }]}
         image="https://ipfs.thirdweb.com/ipfs/bafybeieuonnl7bixecbwolovlrrwdtdpnawdevvktmfxzmyssibgmfzloi"
       /> */}
-      <PunkList punkListData={punkListData} />
     </div>
   );
 }
