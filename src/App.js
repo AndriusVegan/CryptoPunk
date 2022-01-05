af315239f8cd20614447f8d20dff2231dc71096a
AndriusVegan <lifecoachandy7@gmail.com>
2022-01-05 14:01:32 +0200

    fixed all bugs now displaying from opensea API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import CollectionCard from "./components/CollectionCard";
+// import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -11,7 +11,7 @@ function App() {
   useEffect(() => {
     const getMyNfts = async () => {
       const openseaData = await axios.get(
-        "https://testnets-api.opensea.io/assets?asset_contract_address=0xb4E553d06F4be77FB378D1C1E20C138711221312&oder_direction=asc"
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0xb4E553d06F4be77FB378D1C1E20C138711221312&order_direction=asc"
       );
       console.log(openseaData.data.assets);
       setPunkListData(openseaData.data.assets);
@@ -22,12 +22,12 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <CollectionCard
+      {/* <CollectionCard
         id={0}
         name={"VeganPunk"}
         traits={[{ value: 7 }]}
         image="https://ipfs.thirdweb.com/ipfs/bafybeieuonnl7bixecbwolovlrrwdtdpnawdevvktmfxzmyssibgmfzloi"
-      />
+      /> */}
       <PunkList punkListData={punkListData} />
     </div>
   );
